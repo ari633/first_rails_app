02ce2d46ff92238200b99ea1e63d452eaa3591be
ari pratomo <ari633@gmail.com>
2014-05-21 19:45:39 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
        
       # Sign the user in and redirect to the user's show page.
-      
+      flash[:success] = "Welcome to the Sample App!"
       sign_in user
-      redirect_to user
+      redirect_back_or user
       
     else
       flash[:error] = 'Invalid email/password combination' # Not quite right!       
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 	
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -12,11 +20,32 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)    # Not the final implementation!
     if @user.save
       # Handle a successful save.
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])    
+  end
+
+  def update()
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #Handle successfull update
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
 
   private 
@@ -24,6 +53,24 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 
   	end
+
+    # Before filters
+    def signed_in_user
+      unless signed_in?
+        store_location
+        flash[:notice] = "Please sign in."
+        redirect_to signin_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end    
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end    
 
 
 end
